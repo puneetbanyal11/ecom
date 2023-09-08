49e0d6270005cd492ad585aebaf0eeb00969b899
Puneet Banyal <puneetbanyal@Puneets-MacBook-Pro.local>
2023-09-08 17:22:34 +0530

    First commit with Momorepo setup

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,35 @@
+// import Image from "next/image";
+// import { Card } from "ui";
+import styles from "./page.module.css";
+
+// function Gradient({
+//   conic,
+//   className,
+//   small,
+// }: {
+//   small?: boolean;
+//   conic?: boolean;
+//   className?: string;
+// }): JSX.Element {
+//   return (
+//     <span
+//       className={[
+//         styles.gradient,
+//         conic ? styles.glowConic : undefined,
+//         small ? styles.gradientSmall : styles.gradientLarge,
+//         className,
+//       ]
+//         .filter(Boolean)
+//         .join(" ")}
+//     />
+//   );
+// }
+
+
+export default function Page(): JSX.Element {
+  return (
+    <main className={styles.main}>
+      <>APP2</>
+    </main>
+  );
+}
